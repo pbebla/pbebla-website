401dd078319ba0bf180b1565d86fbfc22bc0d256
pbebla <sportsman2325@yahoo.com>
2020-07-18 14:03:25 -0700

    Updated Jumbotron component, fixed image display

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {Jumbotron as Jumbo, Container, Carousel} from 'react-bootstrap';
 import styled from 'styled-components';
-import VVVVVV from '../assets/VVVVVV.jpg';
-import Pokemon from "../assets/Pokemon.jpg";
-import MiniNinjas from "../assets/Mini Ninjas.jpg";
-import Zelda from "../assets/Zelda.jpg";
-import Assassin from "../assets/Assassin's Creed Brotherhood.jpg";
 import Cyna from "../assets/Attempt3.png";
 
 const Styles=styled.div`
     .jumbotron {
-        background: url(${Cyna}) no-repeat fixed bottom;
+        background: url(${Cyna}) no-repeat;
         background-size: cover;
+        background-position: center;
         color: #ccc;
         height: 200px;
         position: relative;
@@ -38,43 +34,7 @@ export const Jumbotron = () => (
                 <p>VVVVV</p>
             </Container>
         </Jumbo>
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={VVVVVV}
-                    alt="First"
-                />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Pokemon}
-                    alt="Second"
-                />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={MiniNinjas}
-                    alt="Third"
-                />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Zelda}
-                    alt="Fourth"
-                />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Assassin}
-                    alt="Fifth"
-                />
-            </Carousel.Item>
-        </Carousel>
+        
 
     </Styles>
 )
