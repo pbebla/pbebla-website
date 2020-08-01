01fafed352cfa73e601b269d2eaba548681f224c
sportsman2325 <sportsman2325@yahoo.com>
2020-07-31 17:13:09 -0700

    updated color of navbar to green

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,41 +1,57 @@
-import React from 'react'
-import {Nav, Navbar} from 'react-bootstrap';
-import styled from 'styled-components';
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
 
 const Styles = styled.div`
-    .navbar {
-        background-color: #222;
+  .navbar {
+    background-color: #326e2e;
+  }
+  .navbar-brand,
+  .navbar-nav .nav-link {
+    color: #bbb;
+    &:hover {
+      color: white;
     }
-    .navbar-brand, .navbar-nav .nav-link {
-        color: #bbb;
-        &:hover {
-            color: white;
-        }
-        &:visited {
-            color: #bbb;
-        }
-        &:active {
-            color: #ffff00;
-        }
+    &:visited {
+      color: #bbb;
     }
+    &:active {
+      color: #ffff00;
+    }
+  }
 `;
 
 export const NavigationBar = () => (
-    <Styles>
-        <Navbar expand="lg">
-            <Navbar.Brand href="/">Pauldin Bebla</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="file:///D:\Users\Pauldin\Downloads\Pauldin_Bebla_Resume.pdf" target="_blank">Resume</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://github.com/pbebla" target="_blank">Github</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="https://www.linkedin.com/in/pauldin-bebla-63b87013a/" target="_blank">Linkedin</Nav.Link></Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </Styles>
-)
-
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Pauldin Bebla</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="https://github.com/pbebla" target="_blank">
+              Github
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              href="https://www.linkedin.com/in/pauldin-bebla-63b87013a/"
+              target="_blank"
+            >
+              Linkedin
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+);
