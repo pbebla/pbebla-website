8ddbd290805b4239fa0480bebd53b68e031e812a
sportsman2325 <sportsman2325@yahoo.com>
2020-07-31 17:12:21 -0700

    Added a template submit email form

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+function simulateNetworkRequest() {
+  return new Promise((resolve) => setTimeout(resolve, 2000));
+}
 
-export const Contact = () => (
+export const Contact = () => {
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => setLoading(true);
+  return (
     <div>
-        <p>Contact</p>
+      <hi>To: pauldinb@gmail.com</hi>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>From:</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlInput2">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control type="subject" placeholder="Subject" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea3">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows="3" />
+        </Form.Group>
+        <Button
+          style={{ backgroundColor: "#326e2e" }}
+          type="submit"
+          disabled={isLoading}
+          onClick={!isLoading ? handleClick : null}
+        >
+          {isLoading ? "Submitting…" : "Submit"}
+        </Button>
+      </Form>
     </div>
-)
+  );
+};
