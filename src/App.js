0d303abca719fa58d7b58428189d1f6d8abeb3c9
sportsman2325 <sportsman2325@yahoo.com>
2020-07-31 17:12:01 -0700

    updated syntax

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Home} from "./Home";
-import {About} from "./About";
-import {Contact} from "./Contact";
-import {NoMatch} from "./NoMatch";
-import {Layout} from './components/Layout';
-import {NavigationBar} from './components/NavigationBar';
-import {Jumbotron} from './components/Jumbotron';
-import Background from './assets/ACS_SC_39_SDCC_EvieAssassination_watermark_211604.0.jpg'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Contact } from "./Contact";
+import { NoMatch } from "./NoMatch";
+import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
+import { Jumbotron } from "./components/Jumbotron";
+import Background from "./assets/ACS_SC_39_SDCC_EvieAssassination_watermark_211604.0.jpg";
 
 function App() {
   return (
-    <div styles={{ backgroundImage:`url(${Background})` }}>
-    <React.Fragment>
-      <NavigationBar />
-      <Jumbotron />
-      <Layout>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/contact" component={Contact}/>
-            <Route component={NoMatch}/>
-          </Switch>
-        </Router>
-      </Layout>
-    </React.Fragment>
+    <div style={{ backgroundImage: `url(${Background})` }}>
+      <React.Fragment>
+        <NavigationBar />
+        <Jumbotron />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
     </div>
   );
 }
