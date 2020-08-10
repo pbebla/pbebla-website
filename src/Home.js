178a1c6c890e92d0adb79bfac6c03b9265013a22
sportsman2325 <sportsman2325@yahoo.com>
2020-08-10 14:27:30 -0700

    Add GitHub Pages config

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import ReactTwitchEmbedVideo  from 'react-twitch-embed-video';
-import { CarouselComponent } from './components/CarouselComponent';
+import React from "react";
+import ReactTwitchEmbedVideo from "react-twitch-embed-video";
+import { CarouselComponent } from "./components/CarouselComponent";
 
 export const Home = () => {
-    return (
-        <div>
-        <CarouselComponent/>
-            <p>Home</p>
-        <ReactTwitchEmbedVideo
-            channel="cynaschism"
-            height="480"
-            muted={false}
-            targetId="twitch-embed"
-            width="100%"
-            layout="video-with-chat"
-        />
-        </div>
-        
-    )
-}
-
+  return (
+    <div>
+      <CarouselComponent />
+      <p>Home</p>
+      <ReactTwitchEmbedVideo
+        channel="lflo203"
+        height="480"
+        muted={false}
+        targetId="twitch-embed"
+        width="100%"
+        layout="video-with-chat"
+      />
+    </div>
+  );
+};
