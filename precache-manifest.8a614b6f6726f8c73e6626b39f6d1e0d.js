ce684309792da4325972db41f48a79e3881a3e4b
sportsman2325 <sportsman2325@yahoo.com>
2020-08-10 14:38:38 -0700

    Updates

---
+++ precache-manifest.8a614b6f6726f8c73e6626b39f6d1e0d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afcc6477d90762dd93816b085fe7ae9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a3b2c636daf26504c7f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b5ac886e6311452f2f0d",
+    "url": "/static/js/2.e63b3be0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e63b3be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3b2c636daf26504c7f",
+    "url": "/static/js/main.f501665d.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "3059ed00c1787aad3c10439e5e8f4d24",
+    "url": "/static/media/ACS_SC_39_SDCC_EvieAssassination_watermark_211604.0.3059ed00.jpg"
+  },
+  {
+    "revision": "30661929e35448d4abc0dd11f8ecbbb4",
+    "url": "/static/media/Assassin's Creed Brotherhood.30661929.jpg"
+  },
+  {
+    "revision": "f6471cde82603153633772d9851df330",
+    "url": "/static/media/Attempt3.f6471cde.png"
+  },
+  {
+    "revision": "521654c7f293c0e0264876640fca8265",
+    "url": "/static/media/Mini Ninjas.521654c7.jpg"
+  },
+  {
+    "revision": "f45781d7e01cbe0f2c0cfac77b3f29f2",
+    "url": "/static/media/Pokemon.f45781d7.jpg"
+  },
+  {
+    "revision": "b4d8d57f530e56c41f12c093b4856cb5",
+    "url": "/static/media/VVVVVV.b4d8d57f.jpg"
+  },
+  {
+    "revision": "14c8d1cc0a71f36f493a573d3a7be3fe",
+    "url": "/static/media/Zelda.14c8d1cc.jpg"
+  }
+]);
